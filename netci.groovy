c689800bf65fc2eac3b6fae3097739cb6ff4a4ed
Enrico Sada <enrico@sada.io>
2016-11-28 18:52:28 +0100

    generate ci jobs for branches too (#50)
    add Debug/Release ci jobs for branches (previously was only for PR)

--- netci.groovy
+++ netci.groovy
@@ -7,7 +7,6 @@ import jobs.generation.Utilities;
 
 def project = GithubProject
 def branch = GithubBranchName
-def isPR = true
 
 def platformList = ['Debian8.2:x64:Debug', 'Ubuntu:x64:Release', 'Ubuntu16.04:x64:Debug', 'OSX:x64:Release', 'Windows_NT:x64:Release', 'Windows_NT:x86:Debug', 'RHEL7.2:x64:Release', 'CentOS7.1:x64:Debug', 'Fedora23:x64:Debug', 'OpenSUSE13.2:x64:Debug']
 
@@ -16,47 +15,49 @@ def static getBuildJobName(def configuration, def os, def architecture) {
 }
 
 
-platformList.each { platform ->
-    // Calculate names
-    def (os, architecture, configuration) = platform.tokenize(':')
+[true, false].each { isPR ->
+    platformList.each { platform ->
+        // Calculate names
+        def (os, architecture, configuration) = platform.tokenize(':')
 
-    // Calculate job name
-    def jobName = getBuildJobName(configuration, os, architecture)
-    def buildCommand = '';
+        // Calculate job name
+        def jobName = getBuildJobName(configuration, os, architecture)
+        def buildCommand = '';
 
-    // Calculate the build command
-    if (os == 'Windows_NT') {
-        buildCommand = ".\\build.cmd -Configuration ${configuration} -Architecture ${architecture} -Targets Default"
-    }
-    else if (os == 'Windows_2016') {
-        buildCommand = ".\\build.cmd -Configuration ${configuration} -Architecture ${architecture} -RunInstallerTestsInDocker -Targets Default"
-    }
-    else if (os == 'Ubuntu') {
-        buildCommand = "./build.sh --skip-prereqs --configuration ${configuration} --docker ubuntu.14.04 --targets Default"
-    }
-    else {
-        // Jenkins non-Ubuntu CI machines don't have docker
-        buildCommand = "./build.sh --skip-prereqs --configuration ${configuration} --targets Default"
-    }
+        // Calculate the build command
+        if (os == 'Windows_NT') {
+            buildCommand = ".\\build.cmd -Configuration ${configuration} -Architecture ${architecture} -Targets Default"
+        }
+        else if (os == 'Windows_2016') {
+            buildCommand = ".\\build.cmd -Configuration ${configuration} -Architecture ${architecture} -RunInstallerTestsInDocker -Targets Default"
+        }
+        else if (os == 'Ubuntu') {
+            buildCommand = "./build.sh --skip-prereqs --configuration ${configuration} --docker ubuntu.14.04 --targets Default"
+        }
+        else {
+            // Jenkins non-Ubuntu CI machines don't have docker
+            buildCommand = "./build.sh --skip-prereqs --configuration ${configuration} --targets Default"
+        }
 
-    def newJob = job(Utilities.getFullJobName(project, jobName, isPR)) {
-        // Set the label.
-        steps {
-            if (os == 'Windows_NT' || os == 'Windows_2016') {
-                // Batch
-                batchFile(buildCommand)
-            }
-            else {
-                // Shell
-                shell(buildCommand)
+        def newJob = job(Utilities.getFullJobName(project, jobName, isPR)) {
+            // Set the label.
+            steps {
+                if (os == 'Windows_NT' || os == 'Windows_2016') {
+                    // Batch
+                    batchFile(buildCommand)
+                }
+                else {
+                    // Shell
+                    shell(buildCommand)
+                }
             }
         }
-    }
 
-    Utilities.setMachineAffinity(newJob, os, 'latest-or-auto')
-    Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
-    //Utilities.addXUnitDotNETResults(newJob, '**/*-testResults.xml', true)
-    Utilities.addGithubPRTriggerForBranch(newJob, branch, "${os} ${architecture} ${configuration} Build")
+        Utilities.setMachineAffinity(newJob, os, 'latest-or-auto')
+        Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
+        //Utilities.addXUnitDotNETResults(newJob, '**/*-testResults.xml', true)
+        Utilities.addGithubPRTriggerForBranch(newJob, branch, "${os} ${architecture} ${configuration} Build")
+    }
 }
 
 
